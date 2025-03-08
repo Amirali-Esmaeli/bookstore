0fd428198d8998f5747195dad1462e377158ae17
Amirali <amiraliesmaeli741@gmail.com>
2025-03-08 14:03:45 +0330

    Configured URL routing for shop app

---
+++ shop/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.book_list, name='book_list'),
+    path('book/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('signup', views.signup, name='signup'),
+    path('cart/', views.cart_view, name='cart_view'),
+    path('cart/add/<int:book_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/remove/<int:book_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('orders/', views.order_history, name='order_history'),
+]
